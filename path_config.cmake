0187610392b491d999d63c4cefe17533d94daeb6
HuYingzhuo(hugo) <hyzboy@gmail.com>
2026-01-28 20:58:29 +0800

    改进cmake路径配置

--- path_config.cmake
+++ path_config.cmake
@@ -1,9 +1,8 @@
 ﻿macro(CMAudioSetup CMAUDIO_ROOT_PATH)
+    message("CMAudio root path: " ${CMAUDIO_ROOT_PATH})
 
-    message("CMAUDIO_ROOT_PATH: " ${CMAUDIO_ROOT_PATH})
-
-    set(CMAUDIO_ROOT_INCLUDE_PATH    ${CMAUDIO_ROOT_PATH}/inc)
-    set(CMAUDIO_ROOT_SOURCE_PATH     ${CMAUDIO_ROOT_PATH}/src)
+    set(CMAUDIO_ROOT_INCLUDE_PATH ${CMAUDIO_ROOT_PATH}/inc)
+    set(CMAUDIO_ROOT_SOURCE_PATH ${CMAUDIO_ROOT_PATH}/src)
 
     include_directories(${CMAUDIO_ROOT_INCLUDE_PATH})
 endmacro()
